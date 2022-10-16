5bc2de9b62fdd463c48152d6e2567b6ccbe79a0a
skcarnes <skylar.carnes@veteransunited.com>
2022-10-15 20:33:22 -0500

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LaunchDarklyService } from './launch-darkly.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'launch-darkly-proto';
+
+  constructor(launchDarklyService: LaunchDarklyService) {}
 }
